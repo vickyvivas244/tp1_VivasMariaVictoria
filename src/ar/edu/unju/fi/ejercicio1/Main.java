cb3d70cf2c6ba76ad140543ce6b48d14381b0c73
usuario <usuario@DESKTOP-84Q6FBK>
2024-04-15 17:26:15 -0300

    Paquetes y clases para ejercicio2

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int h=3;
+		int i=4;
+		int j=2;
+		int k=8;
+		int l=3;
+		float prom= (float)(h+i+j+k+l)/2;
+		
+		System.out.println(h+", "+i+", "+j+", "+k+", "+l+", ");
+		System.out.println("El promedio es:"+prom);
 	}
 
 }
